2d0faa7f5137728d89de1c15ad9490711067dc02
ghamaide <gregoire.hamaide@outlook.com>
2016-04-15 12:10:14 -0400

    add share to bottom buttons

--- app/assets/javascripts/social.js
+++ app/assets/javascripts/social.js
@@ -1,8 +1,17 @@
-$('#facebook-share').click(function() {
+$('#facebook-share-top').click(function() {
   FB.ui({
     method: 'share',
     href: $('meta[property="og:url"]').attr('content'),
   }, function(response){
-    console.log(response);
+    //console.log(response);
+  });
+});
+
+$('#facebook-share-bottom').click(function() {
+  FB.ui({
+    method: 'share',
+    href: $('meta[property="og:url"]').attr('content'),
+  }, function(response){
+    //console.log(response);
   });
 });
